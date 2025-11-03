1c4d8c6912a7ac12523082cc7a9877fa67cb776c
israel <israelashenafi29@gmail.com>
2025-11-03 02:33:14 -0800

    db connection

--- script/loadDb.ts
+++ script/loadDb.ts
@@ -17,5 +17,12 @@ const openAi = new openai({apiKey: OPENAI_API_KEY})
 
 
 const f1Data = [
-    ''
+    'https://www.astu.edu.et/9-about-astu',
+    'https://www.tuumz.com/listing/adama-science-and-technology-university-astu/',
+    'https://en.wikipedia.org/wiki/Adama_Science_and_Technology_University',
+    'https://en.wikipedia.org/wiki/Education_in_Ethiopia',
+    'https://en.wikipedia.org/wiki/Higher_education_in_Ethiopia'
 ]
+
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN);
+const db = client.db(ASTRA_DB_ENDPOINT!, {keyspace: ASTRA_DB_NAMESPACE});
